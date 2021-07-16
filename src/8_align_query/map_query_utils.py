e08d6726a2c637d2c17c675a0fc462d970a38144
Emma Dann <ed6@sanger.ac.uk>
2021-07-16 16:48:59 +0000

    saving work on mapping query to reference

--- src/8_align_query/map_query_utils.py
+++ src/8_align_query/map_query_utils.py
@@ -14,6 +14,8 @@ import scanpy as sc
 import numpy.random as random
 import scipy
 
+import sklearn
+
 def _map_query_to_panfetal(
     query_adata,
     split,
@@ -49,12 +51,13 @@ def _map_query_to_panfetal(
 
     ## Extract gene ids if not
     if not vars_overlap:
-        print("Vars don't overlap -- converting to geneIDs")
-        adata_ref_var = pd.read_csv(scvi_outs_dir + 'PAN.A01.v01.entire_data_normalised_log.{t}.{s}.var.csv'.format(t=timestamp, s=split), index_col=0)
-        adata_ref_var.iloc[var_names_model]['GeneID'].to_csv(scvi_outs_dir + model_dir + "var_names.csv", header=None, index=False)
-        query_adata.var["gene_name"] = query_adata.var_names.copy()
-        query_adata.var_names = query_adata.var[query_gene_id_col]
-        var_names_model = pd.read_csv(scvi_outs_dir + model_dir + "var_names.csv", header=None)[0].values
+        raise ValueError("Vars don't overlap -- convert to geneIDs")
+#         print("Vars don't overlap -- converting to geneIDs")
+#         adata_ref_var = pd.read_csv(scvi_outs_dir + 'PAN.A01.v01.entire_data_normalised_log.{t}.{s}.var.csv'.format(t=timestamp, s=split), index_col=0)
+#         adata_ref_var.iloc[var_names_model]['GeneID'].to_csv(scvi_outs_dir + model_dir + "var_names.csv", header=None, index=False)
+#         query_adata.var["gene_name"] = query_adata.var_names.copy()
+#         query_adata.var_names = query_adata.var[query_gene_id_col]
+#         var_names_model = pd.read_csv(scvi_outs_dir + model_dir + "var_names.csv", header=None)[0].values
     
     ## Add batch column
     if "bbk" not in query_adata.obs.columns:
@@ -84,8 +87,36 @@ def _map_query_to_panfetal(
 
     ## Train
     vae_q.train(max_epochs=200, plan_kwargs=dict(weight_decay=0.0))
-    query_adata_filled.obsm["X_scVI_project"] = vae_q.get_latent_representation()
+    query_adata_filled.obsm["X_scvi"] = vae_q.get_latent_representation()
     return(query_adata_filled, vae_q)
+
+## I/O utils ##
+def _merge_query_and_reference(
+    query_h5ad_file,
+    split,
+    ref_data_dir = '/home/jupyter/mount/gdrive/Pan_fetal/data4gpu_node/',
+    timestamp = '20210429'):
+    '''
+    Read output from query mapping and reference data and merges them in one anndata
+    
+    Params:
+    ------
+    - query_h5ad_file: h5ad file storing mapped query anndata
+    - split: data split ID
+    - ref_data_dir: folder containing reference data
+    - timestamp: dataset timestamp
+    '''
+    query_adata_mapped = sc.read_h5ad(query_h5ad_file)
+
+    ## Read reference
+    ref_adata = sc.read_h5ad(ref_data_dir + 'PAN.A01.v01.entire_data_raw_count.{t}.{s}.h5ad'.format(t=timestamp, s=split))
+    ref_embedding = np.load(ref_data_dir + 'PAN.A01.v01.entire_data_raw_count.{t}.{s}.scVI_out.npy'.format(t=timestamp, s=split))
+    ref_adata.obsm["X_scvi"] = ref_embedding
+    ref_adata.var_names = ref_adata.var.GeneID
+
+    concat_adata = ref_adata.concatenate(query_adata_mapped, batch_key="dataset", batch_categories=["reference", "query"])
+    sc.pp.neighbors(concat_adata, use_rep="X_scvi", n_neighbors = 30)
+    return(concat_adata)
 
 ### Utils to propagate cell type labels ### 
 
@@ -138,8 +169,8 @@ def plot_confusion_mat(adata, query_anno_col, show_low_confidence=True, **kwargs
     col_order = conf_mat.idxmax(0).sort_values().index
     conf_mat = conf_mat[col_order] ## Sort to have some sort of diagonal
     sns.heatmap(conf_mat, xticklabels=True, yticklabels=True, **kwargs);
-    plt.ylabel("Predicted label");
-    plt.xlabel("Query label");
+    plt.xlabel("Predicted label");
+    plt.ylabel("Query label");
 
 def plot_predicted_anno_probability(adata):
     missing_anno = adata.obs["dataset"] == "query"
@@ -148,3 +179,30 @@ def plot_predicted_anno_probability(adata):
     sns.stripplot(data=best_label_df, y="predicted_anno", x="predicted_anno_prob", orient='h', color="black");
     sns.boxplot(data=best_label_df, y="predicted_anno", x="predicted_anno_prob", orient='h', color=None);
     plt.xlabel("probability")
+    
+### Evaluation metrics ### 
+def compute_silhouette(query_adata_mapped, query_anno_col = "annotation_V2"):
+    sil_score_cells = sklearn.metrics.silhouette_samples(query_adata_mapped.obsm["X_scvi"], query_adata_mapped.obs[query_anno_col])
+    query_adata_mapped.obs["sil_score"] = sil_score_cells
+    sil_df = query_adata_mapped.obs[[query_anno_col, "sil_score"]].groupby(query_anno_col).mean()
+    return(sil_df)
+
+def _anno_silhouette_permutation(merged_adata, query_anno_col, n_permutations=1000):
+    query_adata_mapped = merged_adata[merged_adata.obs["dataset"] == "query"]
+    query_adata_mapped.obs[query_anno_col + "_random"] = query_adata_mapped.obs[query_anno_col].copy()
+    rand_sil_df = pd.DataFrame()
+    for i in range(n_permutations):
+        np.random.shuffle(query_adata_mapped.obs[query_anno_col + "_random"].values)
+        rand_sil_df = pd.concat([rand_sil_df, compute_silhouette(query_adata_mapped, query_anno_col + "_random")],1 )
+
+    real_sil_df = compute_silhouette(query_adata_mapped, query_anno_col)
+    for i in range(real_sil_df.shape[0]):
+        real_sil_df["pval"] = 0
+        real_sil_df.iloc[i,1] = sum(rand_sil_df.iloc[0] >= real_sil_df.iloc[0][0])/n_permutations
+    return(real_sil_df)
+
+## Calculate normalized mutual information
+def compute_nmi(query_adata_mapped, query_anno_col):
+    keep_ixs = query_adata_mapped.obs["predicted_anno"] != "low_confidence"
+    nmi=sklearn.metrics.normalized_mutual_info_score(query_adata_mapped.obs[keep_ixs][query_anno_col], query_adata_mapped.obs[keep_ixs]["predicted_anno"])
+    return(nmi)
---
+++ src/8_align_query/run_map_query.py
@@ -0,0 +1,52 @@
+import sys,os
+import scvi
+import anndata
+import numpy as np
+import pandas as pd
+import scanpy as sc
+import scipy
+
+cwd = '.'
+sys.path.append(cwd)
+import map_query_utils
+
+import torch
+device = torch.device("cuda")
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("h5ad_file", help="path to query anndata file")
+parser.add_argument("split_name", help="ID for data split (e.g. NKT, Progenitors, Stroma...)")
+parser.add_argument("batch_name", 
+                    help='''
+                    how do you want to label the new data you are adding? Used only if query_adata.obs['bbk'] is None
+                    ''')
+parser.add_argument("--indir", 
+                    default="/home/jupyter/mount/gdrive/Pan_fetal/data4gpu_node/",
+                    help="folder containing scvi outputs")
+parser.add_argument("--model_dir", 
+                    default=None,
+                    help="folder containing scvi model (Default: None, uses scvi_{split}_model/)")
+parser.add_argument("--query_gene_id_col", 
+                    default="gene_ids",
+                    help="folder containing scvi outputs")
+args = parser.parse_args()
+
+h5ad_file = args.h5ad_file
+split = args.split_name
+batch_name = args.batch_name
+scvi_outs_dir = args.indir
+model_dir = args.model_dir
+
+query_adata = sc.read_h5ad(h5ad_file)
+
+## Check that varnames are gene ids
+if not query_adata.var_names.str.startswith("ENS").all():
+    raise ValueError('`query_adata.var_names` are not Ensembl geneIDs. Please convert')
+    
+query_adata_mapped, _ = map_query_utils._map_query_to_panfetal(query_adata, split=split,  
+                                            scvi_outs_dir = scvi_outs_dir,
+                                            batch_name=batch_name, model_dir=model_dir)
+
+out_file = '{p}.mapped2{s}.h5ad'.format(p=h5ad_file.strip(".h5ad"), s=split)
+query_adata_mapped.write_h5ad(out_file)

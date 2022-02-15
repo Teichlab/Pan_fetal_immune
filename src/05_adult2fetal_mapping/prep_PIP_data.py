76f76f8a2f393bed6bfba45d8a9f05a2253f8b1b
Emma Dann <ed6@sanger.ac.uk>
2022-02-15 08:42:25 +0000

    saving work on new fetal VS adult mapping

---
+++ src/05_adult2fetal_mapping/prep_PIP_data.py
@@ -0,0 +1,52 @@
+### Prep pan-immune datasets for mapping ###
+import sys,os
+import scvi
+import anndata
+import numpy as np
+import pandas as pd
+import scanpy as sc
+
+pi_adata = sc.read_h5ad('/nfs/team205/cx1/Celltypist/revision_science/data/PIP_global_object_raw_count.h5ad')
+
+## Convert gene names to EnsemblIDs as `var_names` (using old object)
+
+pip_adata_old = sc.read_h5ad('/nfs/team205/ed6/data/Fetal_immune/panimmune_query.h5ad')
+var_new = pi_adata.var.reset_index()
+var_old = pip_adata_old.var.copy()
+var_new.columns = ['gene_names', 'highly_variable']
+var_new = pd.merge(var_new, var_old, how='left')
+pi_adata.var = var_new.copy()
+
+pi_adata.var_names = pi_adata.var['gene_ids'].values.copy()
+## Filter out genes with missing ID
+pi_adata = pi_adata[:,~pi_adata.var_names.isna()].copy()
+
+## Match column name for 10x method and donor
+tr_obs = {'donor_id':'donor', 'chem':"method"}
+pi_adata.obs.columns = [tr_obs[x] if x in tr_obs.keys() else x for x in pi_adata.obs.columns]
+tr_method = {'5v1':'5GEX', '5v2':'5GEX', '3':'3GEX'}
+pi_adata.obs['method'] = [tr_method[x] for x in pi_adata.obs['method']]
+
+### Subset to lymphoid/myeloid cells
+lym_anno = ['Tnaive/CM_CD4', 'Memory B cells', 'Trm_gut_CD8', 'NK_CD16+', 'Teffector/EM_CD4',
+                   'Trm_Th1/Th17', 'Tfh',
+                  'Tem/emra_CD8', 'Naive B cells', 'Trm/em_CD8', 'Tregs',
+                  'NK_CD56bright_CD16-', 'Tnaive/CM_CD8', 'Trm_Tgd',
+                  'Plasma cells', 'MAIT', 'Tgd_CRTAM+',
+                  'Tnaive/CM_CD4_activated', 
+                  'Cycling T&NK', 'ILC3', 'ABCs', 'Cycling',
+                  'GC_B (I)', 'GC_B (II)',
+                  'Pre-B', 'Pro-B', 'Plasmablasts']
+
+mye_anno = ['Classical monocytes','Alveolar macrophages','Mast cells',
+            'Nonclassical monocytes', 'Intermediate macrophages','Erythrophagocytic macrophages', 'Progenitor','DC2', 'pDC', 'Intestinal macrophages','DC1', 'Megakaryocytes', 'migDC']
+
+lymph_pi_adata = pi_adata[pi_adata.obs["Category"].isin(lym_anno)].copy()
+mye_pi_adata = pi_adata[pi_adata.obs["Category"].isin(mye_anno)].copy()
+
+
+lymph_pi_adata.write_h5ad("/nfs/team205/ed6/data/Fetal_immune/panimmune_full_LYMPHOID_query.h5ad")
+lymph_pi_adata.write_h5ad("/home/jovyan/mount/gdrive/Pan_fetal/data4gpu_node/panimmune_full_LYMPHOID_query.h5ad")
+
+mye_pi_adata.write_h5ad("/nfs/team205/ed6/data/Fetal_immune/panimmune_full_MYELOID_query.h5ad")
+mye_pi_adata.write_h5ad("/home/jovyan/mount/gdrive/Pan_fetal/data4gpu_node/panimmune_full_MYELOID_query.h5ad")
---
+++ src/utils/scArches_utils/merge_query_2_reference_v2.py
@@ -0,0 +1,96 @@
+### Merging reference and query and computing embedding ###
+
+import os,sys
+import numpy as np
+import scanpy as sc
+import anndata
+
+def _merge_query_and_reference(
+    query_h5ad_file,
+    split,
+    ref_data_dir = '/home/jovyan/mount/gdrive/Pan_fetal/data4gpu_node/',
+    timestamp = '20210429'):
+    '''
+    Read output from query mapping and reference data and merges them in one anndata
+    
+    Params:
+    ------
+    - query_h5ad_file: h5ad file storing mapped query anndata
+    - split: data split ID
+    - ref_data_dir: folder containing reference data
+    - timestamp: dataset timestamp
+    '''
+    query_adata_mapped = sc.read_h5ad(query_h5ad_file)
+    query_adata_full = sc.read_h5ad(query_h5ad_file.split(".mapped2")[0] + ".h5ad") ## To add genes that are not used in scVI
+    query_adata_full.obsm["X_scvi"] = query_adata_mapped.obsm["X_scvi"].copy()
+    query_adata_full.uns["_scvi"] = query_adata_mapped.uns["_scvi"].copy()
+    
+    ## Read reference
+    ref_adata = sc.read_h5ad(ref_data_dir + 'PAN.A01.v01.entire_data_raw_count.{t}.{s}.h5ad'.format(t=timestamp, s=split))
+    ref_embedding = np.load(ref_data_dir + 'PAN.A01.v01.entire_data_raw_count.{t}.{s}.scVI_out.V2.npy'.format(t=timestamp, s=split))
+    ref_adata.obsm["X_scvi"] = ref_embedding
+    ref_adata.var_names = ref_adata.var.GeneID
+
+    concat_adata = anndata.concat([ref_adata, query_adata_full], axis=0,
+                                  label="dataset", keys=["reference", "query"],
+                                  join="outer", merge="unique", uns_merge="unique")
+    concat_adata.obs_names = concat_adata.obs_names + "-" + concat_adata.obs["dataset"].astype("str")
+    return(concat_adata)
+
+def _add_all_query_genes(merged_adata, query_adata_full):
+    if not any(merged_adata.var_names.isin(query_adata_full.var_names)):
+        raise ValueError("var_names don't match between query and merged AnnData")
+
+    if not any(query_adata_full.obs_names.str.endswith("-query")):
+        query_adata_full.obs_names = query_adata_full.obs_names + "-query"
+
+    ## Do the merge
+    full_merged_adata = anndata.concat([merged_adata, query_adata_full], axis=1, join="outer", merge="unique", uns_merge="unique")
+
+    ## Check that the number of obs is right
+    if not full_merged_adata.n_obs == merged_adata.n_obs:
+        raise AssertionError("The number of obs doesn't match, something is wrong in your join")
+
+    ## Check that you have more expression vals than before
+    one_query_cell = query_adata_full.obs_names[10]
+    if not len(full_merged_adata[one_query_cell].X.nonzero()[0]) > len(merged_adata[one_query_cell].X.nonzero()[0]):
+        raise AssertionError("You have less or the same expression values for query cells than before. Are you sure that query_adata_full is the dataset before feature selection?")
+
+    return(full_merged_adata)
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("query_h5ad_file", help="path to query anndata file")
+parser.add_argument("split_name", help="ID for data split (e.g. NKT, Progenitors, Stroma...)")
+parser.add_argument("--ref_data_dir", 
+                    default="/home/jovyan/mount/gdrive/Pan_fetal/data4gpu_node/",
+                    help="folder containing reference data")
+parser.add_argument("--timestamp", 
+                    default="20210429",
+                    help="data time stamp")
+parser.add_argument("--k_neighbors", 
+                    default=30, type=int,
+                    help="number of neighbors")
+args = parser.parse_args()
+
+query_h5ad_file = args.query_h5ad_file
+split = args.split_name
+ref_data_dir = args.ref_data_dir
+timestamp = args.timestamp
+k_neighbors = args.k_neighbors
+
+## Merge datasets
+print("Merging reference and query...\n")
+merged_adata = _merge_query_and_reference(query_h5ad_file, split, ref_data_dir=ref_data_dir)
+query_adata_full = sc.read_h5ad(query_h5ad_file.split(".mapped2")[0] + ".h5ad") ## To add genes that are not used in scVI
+# merged_adata = _add_all_query_genes(merged_adata, query_adata_full)
+
+## Compute UMAP
+print("Running KNN search...\n")
+sc.pp.neighbors(merged_adata, n_neighbors=k_neighbors, use_rep="X_scvi")
+print("Running UMAP...\n")
+sc.tl.umap(merged_adata, min_dist = 0.01, spread = 2)
+
+## Save 
+print("Saving file...\n")
+merged_adata.write_h5ad(query_h5ad_file.split(".h5ad")[0] + ".withReference.h5ad")
